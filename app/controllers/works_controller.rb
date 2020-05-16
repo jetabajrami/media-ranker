c57b12e0657ed2eaaeb99d31798740a99255d785
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-16 04:35:24 -0400

    add avtion crate work. add views.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,39 @@ class WorksController < ApplicationController
    @works = Work.all
   end
 
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      redirect_to user_path(user_id)
+      return
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
