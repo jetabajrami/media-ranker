d50b35ba5b2562dd4e4e039ded33fc8a194d0d04
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-17 03:13:54 -0400

    add validation to work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,6 +2,12 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
 
+  #validation
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true, length: {is: 4}, uniqueness: true
+  validates :description, presence: true
+
   def self.top_ten(category)
     works = Work.where(category: category)
     return works.limit(10)
