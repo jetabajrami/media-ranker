99fa8e2cccace1b631ec830e7b29574bba990b67
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-17 02:42:37 -0400

    add view and costum method for top ten

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,15 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+
+  def self.top_ten(category)
+    works = Work.where(category: category)
+    return works.limit(10)
+  end
+
+  def self.spot_light
+    works = Work.all
+    return works.first
+  end
+
 end
