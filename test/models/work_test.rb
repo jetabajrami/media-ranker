f81f7dab877657ca7ca6c203f254eb53c0e047a2
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-17 22:00:59 -0400

    if work will have the required fields

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -5,7 +5,16 @@ describe Work do
     Work.new(category: "movie", title: "A Beautiful Mind", creator: "Akiva Goldsman", publication_year: "2001" , description: "American biographical drama film based on the life of the American mathematician John Nash.")
   }
 
-  it "can be instantiated" do
+  it "work can be instantiated" do
     expect(new_work.valid?).must_equal true
+  end
+
+  it "work will have the required fields" do
+    new_work.save
+    work = Work.first
+    [:category, :title, :creator, :publication_year, :description].each do |field|
+
+      expect(work).must_respond_to field
+    end
   end
 end
