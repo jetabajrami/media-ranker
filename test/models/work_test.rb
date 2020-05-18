563fb808aa5d2a49c2eb613cb5e555d356430b79
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-17 21:57:55 -0400

    test if work can be instantiated

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:new_work) {
+    Work.new(category: "movie", title: "A Beautiful Mind", creator: "Akiva Goldsman", publication_year: "2001" , description: "American biographical drama film based on the life of the American mathematician John Nash.")
+  }
+
+  it "can be instantiated" do
+    expect(new_work.valid?).must_equal true
+  end
 end
