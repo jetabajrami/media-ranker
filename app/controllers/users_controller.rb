09c4213830a6fed1b9117688282e6c2328a0d666
Jetmire Bajrami <jetmirebajrami@Jetmires-MBP.home>
2020-05-20 15:10:31 -0400

    add show action for users contoller and add style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      head :not_found
+      return
+    end
+  end
 
   def login_form
     @user = User.new
