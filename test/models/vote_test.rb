32d72ba88ad975cab7ba7f7deafd5460e9a014a4
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-23 00:48:30 -0700

    add tests for vote ralations

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @work1 = works(:work1)
+    @lak = users(:lak)
+    @vote = Vote.create(work_id: @work1.id, user_id: @lak.id)
+  end
+  describe "relations" do
+    it "vote belongs to one work" do
+      expect(@vote.work).must_be_instance_of Work
+      expect(@vote.work).must_equal @work1
+    end
+    it "vote belongs to one user" do
+      expect(@vote.user).must_be_instance_of User
+      expect(@vote.user).must_equal @lak
+    end 
+  end
 end
