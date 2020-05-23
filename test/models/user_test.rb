4c3f22f9f9e8dc509a1b04d03dbf4c59b20199c0
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-22 22:53:27 -0700

    add tests for user validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe User do
   before do
     @user = User.new(name: "Lak")
+    @lak = users(:lak)
   end
 
   it "user can be instantiated" do
@@ -11,12 +12,32 @@ describe User do
   end
 
   it "user will have the required field" do
-    user = users(:lak)
-
     [:name].each do |field|
-      expect(user).must_respond_to field
+      expect(@lak).must_respond_to field
     end
   end
 
+  describe "validations" do
+    it "it's invalid when user does't have name" do
  
+      @lak.name = nil
+      expect(@lak.valid?).must_equal false
+      expect(@lak.errors.messages).must_include :name
+      expect(@lak.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "it's valid when user has name" do
+      @lak.name = "lak"
+      expect(@lak.valid?).must_equal true
+    end
+
+
+    it "is invalid with a non-unique name" do
+      @user.name = "lak"
+
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :name
+      expect(@user.errors.messages[:name]).must_equal ["has already been taken"]
+    end
+  end
 end
